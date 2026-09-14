a1f97f31923d96cb82cb2a5d0e1e39f3f4689fef
darrudi <ali.daroudi@gmail.com>
2026-09-14 09:08:00 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Course from "./components/Course";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Course />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
