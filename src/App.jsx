c4963eb1bce7d871b7c8919e6d7be46816a434d8
darrudi <ali.daroudi@gmail.com>
2026-09-15 10:01:06 +0330

    refactor: move Footer component into subdirectory
    Relocate Footer component from `components/Footer` to `components/footer/Footer`,
    including the associated CSS, and update the import path in App.jsx accordingly.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Course from "./components/Course";
-import Footer from "./components/Footer";
+import Footer from "./components/footer/Footer";
 import Header from "./components/Header";
 
 function App() {
